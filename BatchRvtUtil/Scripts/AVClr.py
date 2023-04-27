ca7ec18d3a1acc389a284a51836f7ea29278718a
Andreas Glærum <aglaerum@gmail.com>
2023-04-27 12:36:37 +0200

    Ok

---
+++ BatchRvtUtil/Scripts/AVClr.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+import clr
+import os
+
+def runnin_in_pycharm():
+    if 'PYCHARM_HOSTED' in os.environ:
+        print ("Script is running in PyCharm")
+        return True
+    else:
+        print("Script is not running in PyCharm")
+        return False
+
+def clr_batchrvtutil():
+    if runnin_in_pycharm():
+        # clr.AddReferenceToFileAndPath(r"C:\Users\andreas.glarum\OneDrive - Asplan Viak\Documents\GitHub\AVRevitBatchProcessor\BatchRvtUtil\bin\x64\Release\BatchRvtUtil.dll")
+        clr.AddReferenceToFileAndPath(r"C:\Users\andreas.glarum\OneDrive - Asplan Viak\Documents\GitHub\AVRevitBatchProcessor\BatchRvtUtil\BatchRvtUtil.dll")
+        print "BatchRvtUtil assembly reference added."
+
+clr_batchrvtutil()
+import BatchRvtUtil
+
+
+def get_installed_revit_paths():
+    installed_versions = BatchRvtUtil.RevitVersion.GetInstalledRevitVersions()
+    paths = []
+    for version in installed_versions:
+        path = BatchRvtUtil.RevitVersion.GetRevitExecutableFolderPath(version)
+        paths.append(path)
+    return paths
+
+def get_highest_revit_version_path():
+    installed_revits = BatchRvtUtil.RevitVersion.GetInstalledRevitVersions()
+    installed_revits = sorted(installed_revits, key=lambda x: x.ToString(), reverse=True)
+    latest_revit = installed_revits[0]
+    revit_folder = BatchRvtUtil.RevitVersion.GetRevitExecutableFolderPath(latest_revit)
+    print("Highest Revit version found: " + revit_folder)
+    return revit_folder
+
+def clr_highest_revitapi():
+    if runnin_in_pycharm():
+        revitpath = get_highest_revit_version_path()
+        revitapi_path = os.path.join(revitpath, 'revitapi.dll')
+        print("Adding reference to highest revitapi.dll: " + revitapi_path)
+        clr.AddReferenceToFileAndPath(revitapi_path)
+
+def clr_spesific_revitapi(version):
+    version = str(version)
+    if runnin_in_pycharm():
+        version = BatchRvtUtil.RevitVersion.GetSupportedRevitVersion(version)
+        revitpath = BatchRvtUtil.RevitVersion.GetRevitExecutableFolderPath(version)
+        revitapi_path = os.path.join(revitpath, 'revitapi.dll')
+        print("Adding reference to spesific revitapi.dll: " + revitapi_path)
+        clr.AddReferenceToFileAndPath(revitapi_path)
+
+clr_highest_revitapi()
--- BatchRvtUtil/Scripts/AVServerFuncs.py
+++ BatchRvtUtil/Scripts/AVServerFuncs.py
@@ -2,114 +2,173 @@
 
 import os
 import re
-from datetime import datetime
-import os.path as op
-import time
-import AVFunksjoner as avf
-avf.clr_highest_revitapi()
-from Autodesk.Revit import DB
+import traceback
+
+import batch_rvt_util
 import rpws
+import datetime
+import subprocess
+from Autodesk.Revit import DB
+
 
 RSERVER_MAX_NAME_LEN = 40  # max for modellnavn
 RSERVER_MAX_FOLDER_LEN = 110  # max for mappenavn
-
-""" Baner """
-# httpRserverAdmin = "http://av-revit1/RevitServeradmin{}/".format(str(HOST_APP.version))
 
 
 TEMPLATE_MAPPE = "ikke bruk"
 revit_lokalfiler = "Revit Lokalfiler"  # mappe på egen disk
 
+def get_outputfolder(source_path, output_ifc_folder, output):
+    """ Get output folder for the ifc file. """
 
-# def get_rserverstart(version):
-#     RSERVER_START = r"RSN://av-revit1"
-#     """ It skiftet servernavn fra 2022 version, skal fungere fremover med mindre de endrer servernavn"""
-#     if int(version) >= 2022:
-#         RSERVER_START = r"RSN://AV-Revit{}".format(str(version))
-#     return RSERVER_START
+    node_path = get_nodepath(source_path)
+    ifc_folder = os.path.join(output_ifc_folder, node_path)
+    ifc_folder = os.path.dirname(ifc_folder)
+    return ifc_folder
 
-# def get_rserverstart(version):
-#     RSERVER_START = r"RSN://av-revit1"
-#     """ It skiftet servernavn fra 2022 version, skal fungere fremover med mindre de endrer servernavn"""
-#     if int(version) >= 2022:
-#         RSERVER_START = r"RSN://AV-Revit{}".format(str(version))
-#     return RSERVER_START
+    # node_path = avsf.get_nodepath(source_path)
+    # source_path = os.path.dirname(source_path)
+    #
+    # relative_input_path = os.path.relpath(source_path, input_rvt_folder)
+    # output_folder_path = os.path.join(output_ifc_folder, relative_input_path)
+    # if not os.path.exists(output_folder_path):
+    #     os.makedirs(output_folder_path)
+    # return output_folder_path
+
+def get_revit_file_version(path):
+    # type: (str) -> str
+    if path.startswith(r"\\"):
+        version = get_RserverVersion(path)
+        return version
+    else:
+        fileinfo = DB.BasicFileInfo.Extract(path)
+        return str(fileinfo.Format)
 
 def get_rserverstart(unc_path):
     server_name = get_RserverName(unc_path)
     RSERVER_START = r"RSN://{}".format(unicode(server_name))
     return RSERVER_START
 
-# def NodeToFullServerPath(unc_path):
-#
-#     serverversion = get_RserverVersion(unc_path)
-#     rserverstart = get_rserverstart(unc_path)
-#     full_path = os.path.join(rserverstart, node_path)
-#     assert DB.ModelPathUtils.IsValidUserVisibleFullServerPath(full_path)
-#     fullpath = DB.ModelPathUtils.ConvertUserVisiblePathToModelPath(full_path)
-#     return DB.ModelPathUtils.ConvertModelPathToUserVisiblePath(fullpath)
 
 def get_full_rsn_path(unc_path):
     """ Get the full path to the model on the server """
-    projsubfolder = get_projects_subfolder(unc_path)
+    projsubfolder = get_nodepath(unc_path)
     rserverstart = get_rserverstart(unc_path)
     full_path = os.path.join(rserverstart, projsubfolder)
     assert DB.ModelPathUtils.IsValidUserVisibleFullServerPath(full_path)
     fullpath = DB.ModelPathUtils.ConvertUserVisiblePathToModelPath(full_path)
-    return DB.ModelPathUtils.ConvertModelPathToUserVisiblePath(fullpath)
+    user_visible = DB.ModelPathUtils.ConvertModelPathToUserVisiblePath(fullpath)
+    return unicode(user_visible)
 
 
-def get_projects_subfolder(unc_path):
+def get_nodepath(unc_path):
     """ Get the subfolder name for the projects folder """
-    components = unicode(unc_path).split(os.sep)
+    components = unc_path.split(os.sep)
     projects_index = components.index('Projects')
     projects_path = os.path.join(*components[projects_index + 1:])
     return projects_path
 
-def get_nodepath(unc_path):
-    """ Get the node path from the full path """
-    components = unicode(unc_path).split(os.sep)
-    projects_index = components.index('Projects')
-    projects_path = os.path.join(*components[:projects_index + 1])
-    return projects_path
-
 def get_RserverVersion(unc_path):
-    # Get the version number using regex from a string like \Revit Server 2022\
-    res = re.search(r"\\Revit Server (\d+)\\", unc_path)
-    if not res:
+    try:
+        # Get the version number using regex from a string like \Revit Server 2022\
+        res = re.search(r"\\Revit Server (\d+)\\", unc_path, flags=re.IGNORECASE)
+        return str(res.group(1))
+    except Exception as e:
+        traceback.print_exc()
         return None
-    return str(res.group(1))
+
 
 def get_RserverName(unc_path):
     path_split = os.path.splitunc(unc_path)
     firstpart = path_split[0]
     # Get the name of the server using regex from a string like \\av-revit1\Autodesk
     res = re.search(r"\\\\(.*)\\", firstpart, flags=re.IGNORECASE)
-    if not res:
-        return None
     return str(res.group(1))
+
 
 class ServerPath(object):
     def __init__(self, path):
         super(ServerPath, self).__init__()
-        self.path = path
-        self.servername = get_RserverName(path)
-        self.serverversion = get_RserverVersion(path)
-        self.serverstart = get_rserverstart(path)
-        self.rsn_path = get_full_rsn_path(path)
-        self.server_node = get_projects_subfolder(path)
+        self.path = os.path.normpath(unicode(path).strip())
+        self._servername = None
+        self._serverversion = None
+        self._serverstart = None
+        self._rsn_path = None
+        self._server_node_path = None
         self._rserver = None
 
     def get_rserver(self):
         if self._rserver is None:
             self._rserver = rpws.RevitServer(self.servername, self.serverversion)
+            assert self._rserver.version == self.serverversion
         return self._rserver
 
+    @property
+    def servername(self):
+        if self._servername is None:
+            self._servername = get_RserverName(self.path)
+        return self._servername
+
+    @property
+    def serverversion(self):
+        if self._serverversion is None:
+            self._serverversion = get_RserverVersion(self.path)
+        return self._serverversion
+
+    @property
+    def serverstart(self):
+        if self._serverstart is None:
+            self._serverstart = get_rserverstart(self.path)
+        return self._serverstart
+
+    @property
+    def rsn_path(self):
+        if self._rsn_path is None:
+            self._rsn_path = get_full_rsn_path(self.path)
+        return self._rsn_path
+
+    @property
+    def server_node_path(self):
+        if self._server_node_path is None:
+            self._server_node_path = get_nodepath(self.path)
+        return self._server_node_path
+
     def get_last_modified(self):
-        modified = "Ukjent"
         server = self.get_rserver()
-        try:
-            modified = server.getmodelinfo(r"RSN://AV-Revit2022/2022/000000-00 test123/000000-00 Test123 RIE.rvt").date_modified
-        except:
-            pass
+        modified = server.getmodelinfo(self.server_node_path)  # type: rpws.server.models.ModelInfoEx
+        modified = modified.date_modified
         return modified
+
+    def get_hours_since_modified(self):
+        modified = self.get_last_modified()
+        hours = (datetime.datetime.now() - modified).total_seconds() / 3600
+        return int(hours)
+
+    def get_rservertool(self):
+        tail_path = "RevitServerToolCommand\RevitServerTool.exe"
+        supported_version = batch_rvt_util.RevitVersion.GetSupportedRevitVersion(self.serverversion)
+        revit_installation = batch_rvt_util.RevitVersion.GetRevitExecutableFolderPath(supported_version)
+        rservertool_path = os.path.join(revit_installation, tail_path)
+        assert os.path.exists(rservertool_path)
+        return rservertool_path
+
+    def get_local_path(self, local_rvt_folder):
+        server_node_path = self.server_node_path
+        local_path = os.path.join(local_rvt_folder, server_node_path)
+        return local_path
+
+    def create_local(self, local_folder, output):
+        # type: (ServerPath, function) -> str
+        central_path = self.server_node_path
+        server_name = self.servername
+        local_rvt_path = self.get_local_path(local_folder)
+
+        dest_folder = os.path.dirname(local_rvt_path)
+        if not os.path.exists(dest_folder):
+            os.makedirs(dest_folder)
+
+        rserver_tool = self.get_rservertool()
+        output("Laster ned:\n{}\ntil:\n{}".format(self.path, local_rvt_path))
+        args = [rserver_tool, "createLocalRVT", central_path, "-s", server_name, "-d", local_rvt_path, "-o"]
+        shell = subprocess.check_output(args, shell=True)
+        # output(shell)
