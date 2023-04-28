1ede3ad0cb11d3a697b2779e0962b47184ea469d
Andreas Glærum <aglaerum@gmail.com>
2023-04-28 13:41:05 +0200

    ok

--- BatchRvtUtil/Scripts/AVClr.py
+++ BatchRvtUtil/Scripts/AVClr.py
@@ -5,17 +5,17 @@ import os
 
 def runnin_in_pycharm():
     if 'PYCHARM_HOSTED' in os.environ:
-        print ("Script is running in PyCharm")
+        # print ("Script is running in PyCharm")
         return True
     else:
-        print("Script is not running in PyCharm")
+        # print("Script is not running in PyCharm")
         return False
 
 def clr_batchrvtutil():
     if runnin_in_pycharm():
         # clr.AddReferenceToFileAndPath(r"C:\Users\andreas.glarum\OneDrive - Asplan Viak\Documents\GitHub\AVRevitBatchProcessor\BatchRvtUtil\bin\x64\Release\BatchRvtUtil.dll")
         clr.AddReferenceToFileAndPath(r"C:\Users\andreas.glarum\OneDrive - Asplan Viak\Documents\GitHub\AVRevitBatchProcessor\BatchRvtUtil\BatchRvtUtil.dll")
-        print "BatchRvtUtil assembly reference added."
+        # print "BatchRvtUtil assembly reference added."
         # System.AppDomain.Unload(System.AppDomain.CurrentDomain)
 
 
@@ -34,19 +34,19 @@ def get_highest_revit_version_path():
     installed_revits = sorted(installed_revits, key=lambda x: x.ToString(), reverse=True)
     latest_revit = installed_revits[0]
     revit_folder = BatchRvtUtil.RevitVersion.GetRevitExecutableFolderPath(latest_revit)
-    print("Highest Revit version found: " + revit_folder)
+    # print("Highest Revit version found: " + revit_folder)
     return revit_folder
 
 def clr_revitapi():
     if runnin_in_pycharm():
-        print("Adding reference to revitapi...")
+        # print("Adding reference to revitapi...")
         clr.AddReference("RevitAPI")
 
 def clr_highest_revitapi():
     if runnin_in_pycharm():
         revitpath = get_highest_revit_version_path()
         revitapi_path = os.path.join(revitpath, 'revitapi.dll')
-        print("Adding reference to highest revitapi.dll: " + revitapi_path)
+        # print("Adding reference to highest revitapi.dll: " + revitapi_path)
         clr.AddReferenceToFileAndPath(revitapi_path)
 #
 # def clr_spesific_revitapi(version):
